125885c0af309ab8ad2f509800912874ebb87b77
boisselle <smb@gatech.edu>
2022-02-23 14:17:06 -0800

    finished module 05

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -10,28 +10,30 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const group = new THREE.Group();
+group.position.y = 1;
+group.rotation.y = 1;
+scene.add(group);
 
-// Position
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-mesh.position.set(0.7, -0.6, 1);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+group.add(cube1);
 
-// Scale
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+group.add(cube2);
 
-// Rotation
-// Do the reorder before changing the rotation. This will help prevent a gimbal lock
-mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper();
@@ -39,8 +41,9 @@ const axesHelper = new THREE.AxesHelper();
 scene.add(axesHelper);
 
 // console.log(mesh.position.distanceTo(camera.position));
-mesh.position.normalize();
-console.log(mesh.position.length());
+
+// mesh.position.normalize();
+// console.log(mesh.position.length());
 
 /**
  * Sizes
@@ -58,8 +61,6 @@ camera.position.z = 3;
 // camera.position.y = 1;
 // camera.position.x = 1;
 scene.add(camera);
-
-camera.lookAt(mesh.position);
 
 /**
  * Renderer
